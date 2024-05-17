e1c1d4f377e6d98c4b06e0c2808a33a5024fe769
Cristina Grau <crigraiba@gmail.com>
2024-05-17 18:52:51 +0200

    log requests

--- kotlin-workshop/src/main/kotlin/com/learning/controller/EmployeeController.kt
+++ kotlin-workshop/src/main/kotlin/com/learning/controller/EmployeeController.kt
@@ -3,6 +3,7 @@ package com.learning.controller
 import com.learning.domain.Employee
 import com.learning.service.EmployeeService
 import jakarta.validation.Valid
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,12 +18,18 @@ class EmployeeController(
     private val employeeService: EmployeeService
 ) {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun save(@Valid @RequestBody requestBody: Employee) =
+    fun save(@Valid @RequestBody requestBody: Employee) {
+        log.info("EmployeeController#save, requestBody = $requestBody")
         employeeService.save(requestBody)
+    }
 
     @GetMapping
-    fun findAll(): List<Employee> =
-        employeeService.findAll()
+    fun findAll(): List<Employee> {
+        log.info("EmployeeController#findAll")
+        return employeeService.findAll()
+    }
 }
